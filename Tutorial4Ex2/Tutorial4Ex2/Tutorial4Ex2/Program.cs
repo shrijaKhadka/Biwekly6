698f6fe4f9134e61f494c05f445e5dc5f9b81b73
Naresh Kumar Rai <n.rai@student.fontys.nl>
2023-01-28 16:28:57 +0100

    test commit

--- Tutorial4Ex2/Tutorial4Ex2/Tutorial4Ex2/Program.cs
+++ Tutorial4Ex2/Tutorial4Ex2/Tutorial4Ex2/Program.cs
@@ -16,6 +16,7 @@ namespace Tutorial4Ex2
 
             Console.WriteLine("Address of 1st Cusotmer: " + cust1.id);
             Console.WriteLine("Email ID of 2nd Customer: " + cust2.name);
+             Console.WriteLine("test");
 
             Product p1 = new Product();
             Product p2 = new Product();
